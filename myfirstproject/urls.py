dcf3211725323f7d3b4746955e6482f495782ade
arhigeneral <48457530+arhigeneral@users.noreply.github.com>
2020-10-10 18:14:07 +0300

    Add reverce, remove hello page

--- myfirstproject/urls.py
+++ myfirstproject/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello),
     path('', views.home),
-    path('/reverce', views.reverce),
+    path('reverce/', views.reverce),
 ]
